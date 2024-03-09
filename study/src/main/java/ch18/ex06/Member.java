c1b6ad2028d8f334ac6535efa56ab59910f07885
Choi Hojo <ubermensch100326@gmail.com>
2024-03-10 00:48:13 +0900

    docs: 181강. 18.10 객체 스트림 실습

--- study/src/main/java/ch18/ex06/Member.java
+++ study/src/main/java/ch18/ex06/Member.java
@@ -4,8 +4,11 @@ import java.io.Serializable;
 
 public class Member implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private String id;
     private String name;
+    private int age;
 
     public Member(String id, String name) {
         this.id = id;
